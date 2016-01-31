97e51b272a637af30c7a87c1f41e9c227839b197
Matthew Baggett <matthew@baggett.me>
2016-01-31 21:11:46 +0000

    Genisis

--- mon/lights/philips/index.js
+++ mon/lights/philips/index.js
@@ -19,11 +19,30 @@ console.log("Subscribed to Redis");
 
 var hue = require('node-hue-api');
 
-var displayBridges = function(bridge) {
-    console.log("Hue Bridges Found: " + JSON.stringify(bridge));
+var app = {
+    displayBridges:  function(bridge) {
+        console.log("Hue Bridges Found: " + JSON.stringify(bridge));
+    },
+    connectToBridges: function(bridges){
+        bridges.forEach(app.connectToBridge);
+    },
+    connectToBridge: function(bridge){
+        console.log("Connecting to bridge " + bridge.ipaddress);
+        var username = 'haus' + Math.random();
+        var api = new HueApi(bridge.ipaddress, username);
+        console.log("waiting for button push?");
+        api.config(function(err, config){
+            console.log("api.config callback");
+            if (err) throw err;
+            console.log(JSON.stringify(config, null, 2));
+        });
+    }
 };
 
 hue.nupnpSearch(function(err, result) {
-    if (err) throw err;
-    displayBridges(result);
+    if (err){
+        throw err;
+    }
+    app.displayBridges(result);
+    app.connectToBridges(result);
 });
